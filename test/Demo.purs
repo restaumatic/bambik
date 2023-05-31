92a3107da1f9b6a494a84b1f372fe9e2d4908e7a
erykciepiela <eryk.ciepiela.dev@gmail.com>
2023-05-31 20:03:39 +0000

    web demo

---
+++ test/Demo.purs
@@ -0,0 +1,22 @@
+module Demo where
+
+import Prelude hiding (zero)
+
+import Data.Plus (plus, zero)
+import Data.Invariant.Optics (projection)
+import Effect (Effect)
+import Test.ConsoleWidget (dyntext, immutable, runConsoleWidget, static, text, textInput)
+
+main :: Effect Unit
+main = do
+  let rootWidget
+        = textInput "input1"
+        `plus` (textInput "input2")
+        `plus` (text "some static content" # static)
+        `plus` (dyntext "dyntext" # projection (_ <> "!"))
+        `plus` (text "reset" # static # immutable "new")
+        `plus` zero
+  update <- runConsoleWidget rootWidget mempty
+  update "abc"
+  update "abcd"
+  pure unit
