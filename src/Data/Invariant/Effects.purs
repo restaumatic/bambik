68b0d54fd8464f784383509374ba9060ee84f4f7
erykciepiela <eryk.ciepiela.dev@gmail.com>
2023-06-01 11:30:57 +0000

    cleanup

--- src/Data/Invariant/Effects.purs
+++
@@ -1,9 +0,0 @@
--- Invariant polymorphic transformers - invariant effects
-module Data.Invariant.Effects
-  ( InvTransformer
-  )
-  where
-
-import Data.Invariant (class Invariant)
-
-type InvTransformer i = forall a . Invariant i => i a -> i a
