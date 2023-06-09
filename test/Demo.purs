42df396728cda2854b030c074e7434b4e3b1e3fd
erykciepiela <eryk.ciepiela.dev@gmail.com>
2023-06-09 14:44:11 +0000

    demo

--- test/Demo.purs
+++ test/Demo.purs
@@ -8,8 +8,8 @@ import Data.String (toUpper)
 import Effect (Effect)
 import Specular.Dom.Builder (runMainBuilderInBody)
 import Type.Proxy (Proxy(..))
-import Web (Component, inside, buildComponent, text)
-import Web.MDC (filledText)
+import Web (Component, buildComponent, inside, staticText, text)
+import Web.MDC as MDC
 
 type Order =
       { customer :: Customer
@@ -21,24 +21,23 @@ type Customer =
       }
 
 customer = propertyInvLensTagged (Proxy :: Proxy "customer")
-
 firstName = propertyInvLensTagged (Proxy :: Proxy "firstName")
 lastName = propertyInvLensTagged (Proxy :: Proxy "lastName")
-
 upperCase = projection toUpper
 
 app ∷ Component Order
-app = (
-            filledText "first name" # inside "div" mempty mempty # firstName
-            ^
-            filledText "last name" # inside "div" mempty mempty # lastName
-            ^
-            text # inside "div" mempty mempty # firstName
-            ^
-            text # inside "div" mempty mempty # lastName
-      ) # inside "div" mempty mempty # customer
-      ^
-      text # inside "h4" mempty mempty # upperCase # lastName # customer
+app =
+  (
+    MDC.filledText "first name" # inside "div" mempty mempty # firstName
+    ^
+    MDC.filledText "last name" # inside "div" mempty mempty # lastName
+    ^
+    text # inside "div" mempty mempty # firstName
+    ^
+    text # inside "div" mempty mempty # lastName
+  ) # inside "div" mempty mempty # customer
+  ^
+  staticText "Summary: " ^ text # upperCase # lastName # customer
 
 main :: Effect Unit
 main = do
