584f295ce07f3a5412ebd34d610095c07a0d8796
erykciepiela <eryk.ciepiela.dev@gmail.com>
2023-06-25 23:26:59 +0000

    InvStar

---
+++ src/Data/Invariant/Star.purs
@@ -0,0 +1,37 @@
+module Data.Invariant.Star
+  ( InvStar(..)
+  , Pure
+  )
+  where
+
+import Prelude hiding (zero)
+
+import Data.Either (Either(..), either)
+import Data.Identity (Identity)
+import Data.Invariant (class Cartesian, class CoCartesian, class Invariant)
+import Data.Newtype (class Newtype, unwrap, wrap)
+import Data.Plus (class Plus)
+import Data.Tuple (Tuple(..))
+
+-- basically, invariant version of Star
+newtype InvStar :: (Type -> Type) -> Type -> Type
+newtype InvStar f a = InvStar (a -> f a)
+
+derive instance Newtype (InvStar f a) _
+
+instance Functor f => Invariant (InvStar f) where
+  invmap post pre p = wrap $ pre >>> unwrap p >>> map post
+
+instance Applicative f => Cartesian (InvStar f) where
+  invfirst p = wrap \(Tuple a b) -> Tuple <$> unwrap p a <*> pure b
+  invsecond p = wrap \(Tuple a b) -> Tuple <$> pure a <*> unwrap p b
+
+instance Applicative f => CoCartesian (InvStar f) where
+  invleft p = wrap $ either (unwrap p >>> map Left) (pure <<< Right)
+  invright p = wrap $ either (pure <<< Left) (unwrap p >>> map Right)
+
+instance Monad f => Plus (InvStar f) where
+  plus p1 p2 = wrap $ unwrap p1 >=> unwrap p2
+  zero = wrap pure
+
+type Pure = InvStar Identity
