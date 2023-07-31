beacc75db555c3e8a8216001bdb62e8e5b228bae
erykciepiela <eryk.ciepiela.dev@gmail.com>
2023-07-31 20:42:37 +0000

    cleanup

--- src/Data/Invariant/Transformers.purs
+++ src/Data/Invariant/Transformers.purs
@@ -38,13 +38,39 @@ instance (Applicative f, Plus i) => Plus (ComposeInvInside f i) where
   zero = wrap $ pure zero
 
 
+newtype ComposeInvOutside :: forall k1 k2. (k1 -> Type) -> (k2 -> k1) -> k2 -> Type
+newtype ComposeInvOutside i f a = ComposeInvOutside (i (f a))
+derive instance Newtype (ComposeInvOutside i f a) _
+
+instance (Functor f, Invariant i) => Invariant (ComposeInvOutside i f) where
+  invmap f g = wrap <<< invmap (map f) (map g) <<< unwrap
+
+-- If `i _` can be lifted with lenses, `i (f _)` can be lifted with lenses too as long as `Apply f`
+instance (Apply f, Cartesian i) => Cartesian (ComposeInvOutside i f) where
+  invfirst = wrap <<< invmap (\(Tuple fa fb) -> Tuple <$> fa <*> fb) (\fab -> Tuple (fst <$> fab) (snd <$> fab)) <<< invfirst <<< unwrap
+  invsecond = wrap <<< invmap (\(Tuple fa fb) -> Tuple <$> fa <*> fb) (\fab -> Tuple (fst <$> fab) (snd <$> fab)) <<< invsecond <<< unwrap
+
+-- If `i _` can be lifted with prisms, `i (f _)` can be lifted with prisms too as long as `CoApply f`
+instance (CoApply f, CoCartesian i) => CoCartesian (ComposeInvOutside i f) where
+  invleft = wrap <<< invmap (\efafb -> either (map Left) (map Right) efafb) cozip <<< invleft <<< unwrap
+  invright = wrap <<< invmap (\efafb -> either (map Left) (map Right) efafb) cozip <<< invright <<< unwrap
+
+instance Plusoid i => Plusoid (ComposeInvOutside i f) where
+  plus c1 c2 = wrap $ plus (unwrap c1) (unwrap c2)
+
+instance Plus i => Plus (ComposeInvOutside i f) where
+  zero = wrap zero
+
+instance Tagged i => Tagged (ComposeInvOutside i f) where
+  getPath i = getPath $ unwrap i
+  setPath p i = wrap $ setPath p $ unwrap i
+
 -- Notice `ComposeInvOutside i f a → ComposeInvOutside i f b` parameter is an optic as `i` is an `Invariant` and `f` is a `Functor`.
 -- Lens can be passed if `ComposeInvOutside i f` is `Cartesian` thus if `i` is `Cartesian` and `f` is an `Apply`.
 -- Prism can be passed if `ComposeInvOutside i f` is `CoCartesian` thus if `i` is `CoCartesian and `f` is a `CoApply`.
 -- Composed lens(es) and prism(s) can be passed if `i` is both `Cartesian and `CoCartesian` and `f` is both `Apply` and `CoApply` (e.g. `Identity`).
-invlift ∷ ∀ i f a b. Invariant i => Functor f => (ComposeInvOutside i f a → ComposeInvOutside i f b) → i (f a) → i (f b)
+invlift ∷ ∀ i f a b. (ComposeInvOutside i f a → ComposeInvOutside i f b) → i (f a) → i (f b)
 invlift optic = unwrap <<< optic <<< wrap
-
 
 foo :: forall i733 f734 a735 b736. Invariant i733 => Functor f734 => i733 (f734 a735) -> (ComposeInvOutside i733 f734 a735 -> ComposeInvOutside i733 f734 b736) -> i733 (f734 b736)
 foo inv optic = inv # invlift optic
@@ -73,32 +99,3 @@ liftCustom :: forall i f a b c d
   -> (forall j. Invariant j => j c -> j d)
   -> i (f a) -> i (f d)
 liftCustom prism lens adapter = invlift (prism >>> lens >>> adapter)
-
-
-newtype ComposeInvOutside :: forall k1 k2. (k1 -> Type) -> (k2 -> k1) -> k2 -> Type
-newtype ComposeInvOutside i f a = ComposeInvOutside (i (f a))
-derive instance Newtype (ComposeInvOutside i f a) _
-
-instance (Functor f, Invariant i) => Invariant (ComposeInvOutside i f) where
-  invmap f g = wrap <<< invmap (map f) (map g) <<< unwrap
-
--- If `i _` can be lifted with lenses, `i (f _)` can be lifted with lenses too as long as `Apply f`
-instance (Apply f, Cartesian i) => Cartesian (ComposeInvOutside i f) where
-  invfirst = wrap <<< invmap (\(Tuple fa fb) -> Tuple <$> fa <*> fb) (\fab -> Tuple (fst <$> fab) (snd <$> fab)) <<< invfirst <<< unwrap
-  invsecond = wrap <<< invmap (\(Tuple fa fb) -> Tuple <$> fa <*> fb) (\fab -> Tuple (fst <$> fab) (snd <$> fab)) <<< invsecond <<< unwrap
-
--- If `i _` can be lifted with prisms, `i (f _)` can be lifted with prisms too as long as `CoApply f`
-instance (CoApply f, CoCartesian i) => CoCartesian (ComposeInvOutside i f) where
-  invleft = wrap <<< invmap (\efafb -> either (map Left) (map Right) efafb) cozip <<< invleft <<< unwrap
-  invright = wrap <<< invmap (\efafb -> either (map Left) (map Right) efafb) cozip <<< invright <<< unwrap
-
-instance Plusoid i => Plusoid (ComposeInvOutside i f) where
-  plus c1 c2 = wrap $ plus (unwrap c1) (unwrap c2)
-
-instance Plus i => Plus (ComposeInvOutside i f) where
-  zero = wrap zero
-
-instance Tagged i => Tagged (ComposeInvOutside i f) where
-  getPath i = getPath $ unwrap i
-  setPath p i = wrap $ setPath p $ unwrap i
-
