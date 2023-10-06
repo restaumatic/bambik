047e9f6dcc25b8b455d05510fdb3533aaf38042b
erykciepiela <eryk.ciepiela.dev@gmail.com>
2023-10-06 20:04:01 +0000

    cleanup

--- src/Data/Profunctor/Plus.purs
+++ src/Data/Profunctor/Plus.purs
@@ -11,12 +11,12 @@ import Data.Profunctor (class Profunctor)
 
 class Profunctor p <= ProfunctorPlus p where -- TODO EC find fancier name
     proplus :: forall a . p a a -> p a a -> p a a
-    -- laws:
+    -- law:
     -- proplus a (proplus b c) = proplus (proplus a b) c
 
 class ProfunctorPlus p <= ProfunctorZero p where -- TODO EC find fancier name
     prozero :: forall a. p a a
-    -- laws:
+    -- law:
     --  proplus a prozero == a = proplus prozero a
 
 -- lower precedence than `#` which is 0
