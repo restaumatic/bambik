2d1038219c406d9c854f33922b307d68cbb0477c
erykciepiela <eryk.ciepiela.dev@gmail.com>
2024-03-17 10:01:33 +0000

    demo1 works

--- demo/1/View.purs
+++ demo/1/View.purs
@@ -44,9 +44,9 @@ order = -- T.do
         filledTextField { floatingLabel: constant "Paid" } paid # just # payment
     card S.do
       body1 $ text # constant "Order " <> value >>> shortId <> constant " (uniquely " <> value >>> orderId <> constant ") for " <> value >>> firstName >>> customer <> constant " " <> value >>> lastName >>> customer <> constant " (formally " <> value >>> surname >>> formal >>> customer <> constant " " <> value >>> forename >>> formal >>> customer <> constant ")" <> constant ", fulfilled as " <> (constant "dine in at table " <> value >>> table) >>> slot >>> dineIn >>> fulfillment <> (constant "takeaway at " <> value >>> time) >>> slot >>> takeaway >>> fulfillment <> (constant "delivery to " <> value >>> address) >>> slot >>> delivery >>> fulfillment <> (constant ", paid " <> value >>> paid) >>> slot >>> just >>> payment # slot # debouncer'
-  -- containedButton { label: text # constant "Submit order " <> value >>> shortId >>> debouncer' }
-  -- confirmationDialog { title: text # constant "Submit order " <> value >>> shortId >>> submittedOrder <> constant "?", dismiss: text # constant "No", confirm: text # constant "Yes" } >>> lcmap (\submittedOrder -> { authToken: "", submittedOrder }) $ S.do
-  --   body1 $ text # constant "Authorization required"
-  --   filledTextField { floatingLabel: constant "Auth token" } authToken
-  -- indeterminateLinearProgress # submitOrder
-  -- snackbar { label: text # constant "Order " <> value >>> shortId <> constant " submitted"}
+    containedButton { label: text # constant "Submit order " <> value >>> shortId >>> debouncer' } >>> T.do
+      confirmationDialog { title: text # constant "Submit order " <> value >>> shortId >>> submittedOrder <> constant "?", dismiss: text # constant "No", confirm: text # constant "Yes" } >>> lcmap (\submittedOrder -> { authToken: "", submittedOrder }) $ S.do
+        body1 $ text # constant "Authorization required"
+        filledTextField { floatingLabel: constant "Auth token" } authToken
+      indeterminateLinearProgress # submitOrder
+      snackbar { label: text # constant "Order " <> value >>> shortId <> constant " submitted"}
