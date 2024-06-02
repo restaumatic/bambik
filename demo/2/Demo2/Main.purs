314f89a3430019e3059beaf1854f025c9825f955
erykciepiela <eryk.ciepiela.dev@gmail.com>
2024-06-02 20:38:40 +0000

    demo2

--- demo/2/Demo2/Main.purs
+++ demo/2/Demo2/Main.purs
@@ -1,10 +1,18 @@
 module Demo2.Main (main) where
 
-import Prelude (Unit)
+import Prelude hiding (div)
 
 import Effect (Effect)
-import Demo2.View (view)
-import Web (body)
+import QualifiedDo.Semigroupoid as T
+import Web (a, attr, body, div, li, p, text, ul)
+import Widget (static)
 
 main :: Effect Unit
-main = body view
+main = body $
+  div >>> attr "style" "border: 1px solid silver; padding: 30px;" $ T.do
+    static "Hello World!" $ p $ text
+    ul $ T.do
+      static "One" $ li $ text
+      static "Two" $ li $ text
+      static "Three" $ li $ text
+    static "Link" $ a >>> attr "href" "https://www.google.com" >>> attr "target" "_blank" $ text
