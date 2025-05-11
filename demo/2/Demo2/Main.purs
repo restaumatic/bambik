61a6aa940090fd31f6540a414790cac7a304f72f
erykciepiela <eryk.ciepiela.dev@gmail.com>
2025-05-11 16:21:28 +0000

    Semigroupoid.do for static web

--- demo/2/Demo2/Main.purs
+++ demo/2/Demo2/Main.purs
@@ -3,16 +3,15 @@ module Demo2.Main (main) where
 import Prelude hiding (div)
 
 import Effect (Effect)
-import QualifiedDo.Semigroup as S
+import QualifiedDo.Semigroupoid as T
 import Web (a, body, div, li, p, staticHTML, staticText, ul, (:=))
 
 main :: Effect Unit
-main = body $
-  div >>> "style" := "border: 1px solid silver; padding: 30px;" $ S.do
-    p $ staticText "Hello World!"
-    ul $ S.do
-      li $ staticText "One"
-      li $ staticText "Two"
-      li $ staticText "Three"
-    a >>> "href" := "https://www.google.com" >>> "target" := "_blank" >>> "title" := "Google Search" $ staticText "Link"
-    staticHTML "<hr/>"
+main = body $ div >>> "style" := "border: 1px solid silver; padding: 30px;" $ T.do
+  p $ staticText "Hello World!"
+  ul $ T.do
+    li $ staticText "One"
+    li $ staticText "Two"
+    li $ staticText "Three"
+  a >>> "href" := "https://www.google.com" >>> "target" := "_blank" >>> "title" := "Google Search" $ staticText "Link"
+  staticHTML "<hr/>"
