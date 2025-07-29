52b63344344443e4f88c5c7cc4c03f83a952c167
erykciepiela <eryk.ciepiela.dev@gmail.com>
2025-07-29 15:04:37 +0200

    lookup-insert-map example

--- demo/lookup-insert-map/Main.purs
+++ demo/lookup-insert-map/Main.purs
@@ -1,14 +1,10 @@
+-- Opis by MB:
+-- 
 -- Mam formularz do konfiguracji integracji. Chciałbym mieć w nim takie zachowanie:
 -- Jak wybiorę restaurację i ustawię rodzaj integracji na UberDirect, to pole w tym formularzu powinno sobie ustawić wartość na podstawie wyniku RPC (no i jak zmienię resto, to przeliczyć)
 -- Jak RPC zwróci Nothing to powinien się pojawić guzik przy tym polu, który pozwala odpalić inne RPC, które potencjalnie ustawi tę wartość.
 -- Jak coś takiego osiągnąć w R.Formie? Jedyne co mi przychodzi do głowy to użyć withEffect. Ale w docsach jest do tego dużo ostrzeżeń, a nie znalazłem dobrego przykładu w kodzie. W sumie to bardziej chodzi o tę pierwszą część, bo ta druga to action + merge. A w tej pierwszej jakoś nie bardzo pasuje zrobić readDynamic i withInitialValue.
 -- Kontekst jest taki, że UberDirect ma coś takiego jak organizacja i podorgranizacje. Zamiast prosić użytkowników, żeby wyklikali sobie konto w UberDirect i podali nam swój zestaw credentiali, to możemy stworzyć dla każdej restauracji podorganizacje w ramach naszej organizacji. Pierwsze RPC sprawdza czy z jakąś restauracją jest już powiązana jakaś organizacja i jeśli tak, to uzupełniamy jej id w tym polu. Drugie RPC pozwala na utworzenie nowej organizacji i powiązanie z nią restauracji.
-
--- key text field -> lookup button -> lookup value progress bar --------------------------------------------------------------> value
---                                                              -> create value button -> create value progress bar (effect) -> value
-
--- Key
--- Value
 module Main (main) where
 
 import Prelude hiding (div)
