1ddc0c8be6036cb90d62a88735013a48aa5d4766
erykciepiela <eryk.ciepiela.dev@gmail.com>
2025-10-10 11:26:55 +0200

    cleanup, (->) instances

--- src/Data/Profunctor/StrongLike.purs
+++ src/Data/Profunctor/StrongLike.purs
@@ -2,15 +2,19 @@ module Data.Profunctor.StrongLike where
 
 import Prelude
 
-import Data.Either (Either)
+import Data.Either (Either(..))
 import Data.Profunctor (class Profunctor, lcmap, rmap)
-import Data.Tuple (Tuple)
+import Data.Tuple (Tuple(..))
 
 -- StrongLike
 
 class Profunctor p <= StrongLike p where
   firstlike :: forall s b . p s b -> p s (Tuple b s) -- b introduced, s preserved
   secondlike :: forall s b . p s b -> p s (Tuple s b)  -- b introduced, s preserved
+
+instance StrongLike (->) where
+  firstlike f s = Tuple (f s) s
+  secondlike f s = Tuple s (f s)
 
 -- what about dual `forall a b . p (Tuple a b) b -> p a b`? It's not trivial.
 
@@ -29,11 +33,11 @@ class Profunctor p <= StrongLike p where
 -- Compare to `lens :: forall s t a b. (s -> a) -> (s -> b -> t) -> Lens s t a b` from `profunctor-lenses` library.
 
 -- setter? introducer?
-halflens :: forall p s t b. StrongLike p => (Tuple b s -> t) -> p s b -> p s t
+halflens :: forall s t b. (Tuple b s -> t) -> (forall p. StrongLike p => p s b -> p s t)
 halflens set = firstlike >>> rmap set
 
 -- fieldSetter?
-halffield :: forall p s b. StrongLike p => (Tuple b s -> s) -> p s b -> p s s
+halffield :: forall s b. (Tuple b s -> s) -> (forall p. StrongLike p => p s b -> p s s)
 halffield = halflens
 
 -- with halflens/halffield, `p s b` gets more context (the whole `s`, not only part `b`) which might be useful e.g. for validation new b candidates against the whole s 
@@ -51,6 +55,14 @@ field = lens
 class Profunctor p <= ChoiceLike p where
   leftlike :: forall t a. p a t -> p (Either a t) t -- a eliminated, t preserved
   rightlike :: forall t a. p a t -> p (Either t a) t -- a eliminated, t preserved
+
+instance ChoiceLike (->) where
+  leftlike f = case _ of
+    Left a -> f a
+    Right t -> t
+  rightlike f = case _ of
+    Left t -> t
+    Right a -> f a
 
 -- ChoiceLike is a generalization of Choice
 
