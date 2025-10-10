5b926f4d5f05fa65d9901a641ca7ace60b3f6134
erykciepiela <eryk.ciepiela.dev@gmail.com>
2025-10-10 19:19:03 +0200

    StrongLike/ChoiceLike modules

--- src/Data/Profunctor/StrongLike.purs
+++ src/Data/Profunctor/StrongLike.purs
@@ -38,37 +38,3 @@ instance StrongLike (->) where
 
 halflensDecode :: forall s t b. (forall p. StrongLike p => p Unit b -> p s t) -> Tuple b s -> t
 halflensDecode f (Tuple b s) = f (const b) s
-
--- ChoiceLike
-
-class Profunctor p <= ChoiceLike p where
-  leftlike :: forall t a. p a Void -> p (Either a t) t -- a eliminated, t preserved
-  rightlike :: forall t a. p a Void -> p (Either t a) t -- a eliminated, t preserved
-
--- Is ChoiceLike a generalization of Choice?
-
--- ChoiceLike is enough to encode a half-prism (a.k.a. eliminator) as there is an isomorphism between:
--- `s -> Either a t` and `forall ChoiceLike p. p a Void -> p s t`.
-
-halfprism :: forall s t a. (s -> Either a t) -> (forall p. ChoiceLike p => p a Void -> p s t)
-halfprism eliminate = leftlike >>> lcmap eliminate
-
--- Does it have any sense/application?
-halfprism' :: forall t a. (t -> Either a t) ->(forall p. ChoiceLike p => p a Void -> p t t)
-halfprism' = halfprism 
-
--- Useful ChoiceLike instance for decoding half-prisms
--- Add to profunctors package? 
-newtype Callback r a b = Callback ((b -> r) -> (a -> r))
-
-derive instance Newtype (Callback r a b) _
-
-instance Profunctor (Callback r) where
-  dimap f g r = wrap \br -> (unwrap r) (br <<< g) <<< f
-
-instance ChoiceLike (Callback r) where
-  leftlike r = wrap $ either (unwrap r absurd)
-  rightlike r = wrap $ flip either (unwrap r absurd)
-
-halfprismDecode :: forall s t a. (forall p. ChoiceLike p => p a Void -> p s t) -> s -> Either a t
-halfprismDecode f = unwrap (f (Callback (const Left))) Right
