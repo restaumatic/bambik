541729a5cb68f3564df172e7d94728b6cd5161e3
erykciepiela <eryk.ciepiela.dev@gmail.com>
2025-10-14 17:00:55 +0000

    Cont String and Choice instances

--- src/Data/Profunctor/Cont.purs
+++ src/Data/Profunctor/Cont.purs
@@ -2,8 +2,12 @@ module Data.Profunctor.Cont where
 
 import Prelude
 
+import Data.Either (Either(..))
 import Data.Newtype (class Newtype, unwrap, wrap)
 import Data.Profunctor (class Profunctor)
+import Data.Profunctor.Choice (class Choice)
+import Data.Profunctor.Strong (class Strong)
+import Data.Tuple (Tuple(..))
 
 -- Add to profunctors package?
 newtype Cont r a b = Cont ((b -> r) -> (a -> r))
@@ -12,6 +16,18 @@ derive instance Newtype (Cont r a b) _
 
 instance Profunctor (Cont r) where
   dimap f g r = wrap \br -> (unwrap r) (br <<< g) <<< f
+
+instance Strong (Cont r) where
+  first r = wrap \bd2r (Tuple a d) -> unwrap r (\b -> bd2r (Tuple b d)) a
+  second r = wrap \db2r (Tuple d a) -> unwrap r (\b -> db2r (Tuple d b)) a
+
+instance Choice (Cont r) where
+  left r = wrap \bd2r -> case _ of
+    Left a -> unwrap r (\b -> bd2r (Left b)) a
+    Right d -> bd2r (Right d)
+  right r = wrap \db2r -> case _ of
+    Right a -> unwrap r (\b -> db2r (Right b)) a
+    Left d -> db2r (Left d)
 
 foo :: forall r a b. (a -> b) -> Cont r a b
 foo a2b = wrap (\b2r -> a2b >>> b2r)
