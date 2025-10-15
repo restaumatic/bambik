727cbf26fcc2348b1cf247081510d01e6e5a9d0e
erykciepiela <eryk.ciepiela.dev@gmail.com>
2025-10-15 08:44:37 +0000

    Cont utils

--- src/Data/Profunctor/Cont.purs
+++ src/Data/Profunctor/Cont.purs
@@ -29,11 +29,13 @@ instance Choice (Cont r) where
     Right a -> unwrap r (\b -> db2r (Right b)) a
     Left d -> db2r (Left d)
 
-foo :: forall r a b. (a -> b) -> Cont r a b
-foo a2b = wrap (\b2r -> a2b >>> b2r)
+edit :: forall r a b. (a -> b) -> Cont r a b
+edit a2b = wrap (\b2r -> a2b >>> b2r)
 
-bar :: forall r b. b -> Cont r Unit b
-bar b = foo (const b)
+introduce :: forall r b. b -> Cont r Unit b
+introduce b = edit (const b)
 
-baz :: forall a. Cont a a Void
-baz = wrap (const identity)
+-- a.k.a. "handle"?
+eliminate :: forall a r. (a -> r) -> Cont r a Void
+eliminate a2r = wrap (\_ a -> a2r a)
+
